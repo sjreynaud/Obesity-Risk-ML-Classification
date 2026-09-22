93b3ff73ccebcb062c9d9da144d52669cd549754
Sidney Reynaud, Jr. <168488310+sjreynaud@users.noreply.github.com>
2026-09-21 19:20:09 -0500

    train_qda.py

---
+++ Model_2_Quadratic Discriminant Analysis (QDA)/src/train_qda.py
@@ -0,0 +1,7 @@
+**#Train/validation split**
+
+X_train_qda, X_valid_qda, y_train_qda, y_valid_qda = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+qda_pipeline.fit(X_train_qda, y_train_qda)
